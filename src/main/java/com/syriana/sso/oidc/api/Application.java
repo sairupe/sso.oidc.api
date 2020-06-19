8f3499377af8a45dc79d69a8625dd3bf094b250d
zhongheng <260789672@qq.com>
2020-06-19 18:54:23 +0800

    28:17

--- src/main/java/com/syriana/sso/oidc/api/Application.java
+++ src/main/java/com/syriana/sso/oidc/api/Application.java
@@ -2,12 +2,23 @@ package com.syriana.sso.oidc.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @RestController
+    @RequestMapping("/")
+    class AppController {
+            @GetMapping
+            String hello(){
+                return "12315";
+            }
+    }
 }
