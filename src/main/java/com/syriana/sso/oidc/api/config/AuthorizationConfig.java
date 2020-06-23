98de0b9e2790edd74b77be1895d9bb1279f5d1ba
zhongheng <260789672@qq.com>
2020-06-23 14:57:07 +0800

    【修改】增加password模式校验。和code模式混合

--- src/main/java/com/syriana/sso/oidc/api/config/AuthorizationConfig.java
+++ src/main/java/com/syriana/sso/oidc/api/config/AuthorizationConfig.java
@@ -2,11 +2,15 @@ package com.syriana.sso.oidc.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import sun.plugin2.applet.context.NoopExecutionContext;
 
 /**
  * @author syriana.zh
@@ -19,11 +23,20 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         //允许表单提交
         security.allowFormAuthenticationForClients()
                 .checkTokenAccess("isAuthenticated()");
+//                .tokenKeyAccess("permitAll()");
     }
 
     @Override
@@ -32,7 +45,7 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
         clients.inMemory()
                 .withClient("client-a") //client端唯一标识
                 .secret(passwordEncoder.encode("client-a-secret")) //客户端的密码，这里的密码应该是加密后的
-                .authorizedGrantTypes("authorization_code") //授权模式标识
+                .authorizedGrantTypes("authorization_code", "password") //授权模式标识
                 .scopes("read_user_info") //作用域
                 .resourceIds("resource1") //资源id
                 .redirectUris("http://localhost:9001/callback");//回调地址
