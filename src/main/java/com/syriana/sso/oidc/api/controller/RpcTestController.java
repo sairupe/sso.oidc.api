347975646e5c41723c8dacd7a58bc88599793a63
zhongheng <260789672@qq.com>
2020-11-16 17:10:49 +0800

    【修改】解决跨域问题

--- src/main/java/com/syriana/sso/oidc/api/controller/RpcTestController.java
+++ src/main/java/com/syriana/sso/oidc/api/controller/RpcTestController.java
@@ -3,6 +3,7 @@ package com.syriana.sso.oidc.api.controller;
 import com.syriana.sso.oidc.api.response.common.RestResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import sso.oidc.srv.model.RpcTestResDto;
  * @author syriana.zh
  * @date 2020/09/30
  */
+@CrossOrigin(allowCredentials = "true")
 @Slf4j
 @RestController
 @RequestMapping("/rpc")
